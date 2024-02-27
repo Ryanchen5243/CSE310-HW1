bfaf5c03e59d2aa10b9890629045adff293f3ede
rchen279 <ryan.chen.1@stonybrook.edu>
2024-02-27 02:32:18 -0500

    initial version of proxy

--- proxyserver.py
+++ proxyserver.py
@@ -12,38 +12,87 @@ HOST = 'localhost'
 cache = {}
 
 def handleClient(conn_sock):
+  # read entire request
   request = ""
   while True:
     chunk = conn_sock.recv(BUFFER_SIZE).decode()
     request += chunk
     if "\r\n\r\n" in request: # end of request
       break
-  request_line = request.split("\n")[0].split(" ")
-  print("The data from socket is ",request_line)
-  req_resource = request_line[1][1:]
-  print("the requested resouce is ",req_resource)
+  request_url = request.split("\n")[0].split(" ")[1]
 
-  http_response = ""
-  response_content = ""
-  content_type = "text/html" # default to html
+  # construct HTTP response to send back to requesting client
 
-  if req_resource in cache:
-    # retrieve resource from cache
-    pass
+  if request_url in cache:
+    # check modification date, fetch if new, otherwise retrieve resource from cache
+    print("Requested url {} was found in cache...now retrieving...".format(request_url))
+    # logic for fetching from cache
+
+
   else:
-    # fetch resource from server
-    print("the conns ock sisi s,",conn_sock)
-    pass
+    # fetch resource from server using GET request
+    try:
+      # retrieve domain name
+      domain_name = request_url.split('/')[1]
+      # print("the domain name is ",domain_name)
+      if (domain_name == ''):
+        domain_name = "www.google.com" # default case
+      # print("the domain name is ",domain_name)
 
-  # add header lines
-  http_response += "HTTP/1.1 200 OK\r\n" # status line
-  http_response += f"Content-Type: {content_type}\r\n" # headers
-  http_response += "Content-Length: {}\r\n".format(len(response_content))
-  http_response += "\r\n"
-  conn_sock.sendall(http_response.encode())
-  # # send file content
-  conn_sock.sendall(response_content)
+      # retrieve resource path
+      resource_path = ""
+      if len(request_url.split('/')) > 2:
+        for path_el in request_url.split('/')[2:]:
+          resource_path += "/" + path_el
+      else:
+        resource_path = "/"
+      # print("the retreived resource path is ", resource_path)
+      # done domain name and resource path
 
+      # send GET request to origin server
+      clientSocket = socket(AF_INET, SOCK_STREAM)
+      clientSocket.connect((domain_name,80))
+      get_req_to_origin = f"GET {resource_path} HTTP/1.1\r\n" \
+                    f"Host: {domain_name}\r\n" \
+                    "Connection: keep-alive\r\n" \
+                    "\r\n"
+
+      clientSocket.sendall(get_req_to_origin.encode())
+      print("Requested: {} {} .... fetching response from the origin server".format(domain_name,resource_path))
+
+      origin_server_response = ""
+      while True:
+        data = clientSocket.recv(BUFFER_SIZE)
+        if not data:
+            break
+        origin_server_response += data.decode()
+      clientSocket.close()
+
+      print("The origin server response is {}".format(origin_server_response))
+      print("Now caching... {} -> origin server response".format(request_url))
+      # cache origin server response
+      cache[request_url] = origin_server_response
+      
+      # send response back to client
+      conn_sock.sendall(origin_server_response.encode())
+
+    except gaierror as e:
+      # bad dns lookup (invalid domain name)
+      pass
+
+
+  # http_response = ""
+  # response_content = ""
+  # content_type = "text/html" # default to html
+
+  # # add header lines
+  # http_response += "HTTP/1.1 200 OK\r\n" # status line
+  # http_response += f"Content-Type: {content_type}\r\n" # headers
+  # http_response += "Content-Length: {}\r\n".format(len(response_content))
+  # http_response += "\r\n"
+  # conn_sock.sendall(http_response.encode())
+  # # # send file content
+  # conn_sock.sendall(response_content.encode())
 
 
 def main():
